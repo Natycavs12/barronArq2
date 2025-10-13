148235e3bb19ad7c5db08f26484faed633ed1b8b
Natycavs12 <nbbarron12@gmail.com>
2025-10-13 17:27:58 -0300

    add pagina proys comerciales y servicios

---
+++ src/data/commercials.js
@@ -0,0 +1,144 @@
+export const comerciales = [
+  {
+    categoria: "comerciales",
+    slug: "boulan",
+    nombre: "Panadería Boulan",
+    descripcion: `
+        Obra: Panadería Boulan
+        Dirección: Ugarteche 3045 CABA
+        Trabajos realizados: Construcción 
+        •	80m2 cubiertos
+        Comitente: Sapore Di Pane
+        `,
+    thumbnail: "/assets/commercials/Com_Boulan/frente_boulan.jpg",
+    imagenes: [
+      "/assets/commercials/Com_Boulan/frente_boulan.jpg",
+      "/assets/commercials/Com_Boulan/frente2.JPG",
+      "/assets/commercials/Com_Boulan/frente3.JPG",
+      "/assets/commercials/Com_Boulan/luces.JPG",
+      "/assets/commercials/Com_Boulan/luces2.JPG",
+      "/assets/commercials/Com_Boulan/mueble.JPG",
+      "/assets/commercials/Com_Boulan/mueble_2.JPG",
+      "/assets/commercials/Com_Boulan/rincon.JPG",
+    ],
+  },
+  {
+    categoria: "comerciales",
+    slug: "casa-libertador",
+    nombre: "Casa Libertador – Casa Velatoria",
+    descripcion: `
+        Obra: Casa Libertador – Casa Velatoria
+        Dirección: Congreso 1757 - CABA
+        Trabajos realizados: Proyecto, Dirección y Construcción 
+        •	1350 m2 cubiertos
+        Comitente: Compañía Miguel Costa SRL
+        `,
+    thumbnail: "/assets/commercials/Com_Casa_Libertador/entrada.jpg",
+    imagenes: [
+      "/assets/commercials/Com_Casa_Libertador/entrada.JPG",
+      "/assets/commercials/Com_Casa_Libertador/puerta.jpg",
+      "/assets/commercials/Com_Casa_Libertador/recepcion.jpg",
+      "/assets/commercials/Com_Casa_Libertador/recepcion2.jpg",
+      "/assets/commercials/Com_Casa_Libertador/recepcion3.jpg",
+      "/assets/commercials/Com_Casa_Libertador/sala_estar.jpg",
+      "/assets/commercials/Com_Casa_Libertador/sala_estar2.jpg",
+      "/assets/commercials/Com_Casa_Libertador/sala_estar3.jpg",
+      "/assets/commercials/Com_Casa_Libertador/sala_estar4.jpg",
+      "/assets/commercials/Com_Casa_Libertador/vista_entrada.jpg",
+      "/assets/commercials/Com_Casa_Libertador/vista_recepcion.jpg",
+      "/assets/commercials/Com_Casa_Libertador/sala_velatoria.jpg",
+      "/assets/commercials/Com_Casa_Libertador/ventana.jpg",
+      "/assets/commercials/Com_Casa_Libertador/privado.jpg",
+      "/assets/commercials/Com_Casa_Libertador/toillet.jpg",
+      "/assets/commercials/Com_Casa_Libertador/toillet2.jpg",
+      "/assets/commercials/Com_Casa_Libertador/baños.jpg",
+      "/assets/commercials/Com_Casa_Libertador/cocina.jpg",
+      "/assets/commercials/Com_Casa_Libertador/deco.jpg",
+      "/assets/commercials/Com_Casa_Libertador/fuente.jpg",
+      "/assets/commercials/Com_Casa_Libertador/estar_pag.jpg",
+      "/assets/commercials/Com_Casa_Libertador/estar_pag2.jpg",
+      "/assets/commercials/Com_Casa_Libertador/velatoria_pag.jpg",
+    ],
+  },
+  {
+    categoria: "comerciales",
+    slug: "farmacia-ipharm",
+    nombre: "Farmacia I-Pharm",
+    descripcion: `
+        Obra: Farmacia I Pharm
+        Dirección: Centro comercial NORDELTA
+        Trabajos realizados: Proyecto, Dirección y Construcción 
+        •	650m2 cubiertos
+        Comitente: I Pharm
+        `,
+    thumbnail: "/assets/commercials/Com_I-Pharm/frente_dia.jpg",
+    imagenes: [
+      "/assets/commercials/Com_I-Pharm/frente_dia.jpg",
+      "/assets/commercials/Com_I-Pharm/frente_tarde.jpg",
+      "/assets/commercials/Com_I-Pharm/frente_noche.jpg",
+      "/assets/commercials/Com_I-Pharm/frente_noche2.jpg",
+      "/assets/commercials/Com_I-Pharm/entrada.jpg",
+      "/assets/commercials/Com_I-Pharm/gondolas.jpg",
+      "/assets/commercials/Com_I-Pharm/farmacia.jpg",
+      "/assets/commercials/Com_I-Pharm/atencion_farmacia.jpg",
+      "/assets/commercials/Com_I-Pharm/escalera.jpg",
+      "/assets/commercials/Com_I-Pharm/escalera2.jpg",
+      "/assets/commercials/Com_I-Pharm/consultorio.jpg",
+      "/assets/commercials/Com_I-Pharm/deco.jpg",
+      "/assets/commercials/Com_I-Pharm/deco2.jpg",
+      "/assets/commercials/Com_I-Pharm/interior.jpg",
+      "/assets/commercials/Com_I-Pharm/interior2.jpg",
+      "/assets/commercials/Com_I-Pharm/detalles.jpg",
+    ],
+  },
+  {
+    categoria: "comerciales",
+    slug: "resto-mirlo",
+    nombre: "Mirlo",
+    descripcion: `
+        Obra: Construcción local MIRLO
+        Dirección: Centro Comercial NORDELTA
+        Trabajos realizados: Ejecución completa del local
+        Comitente: MIRLO
+        `,
+    thumbnail: "/assets/commercials/Com_Mirlo/frente.jpg",
+    imagenes: [
+      "/assets/commercials/Com_Mirlo/frente.jpg",
+      "/assets/commercials/Com_Mirlo/frente_noche.jpg",
+      "/assets/commercials/Com_Mirlo/exterior.jpg",
+      "/assets/commercials/Com_Mirlo/entrada.jpg",
+      "/assets/commercials/Com_Mirlo/barra.jpg",
+      "/assets/commercials/Com_Mirlo/salon.jpg",
+      "/assets/commercials/Com_Mirlo/salon_vista_aerea.jpg",
+      "/assets/commercials/Com_Mirlo/salon2.jpg",
+      "/assets/commercials/Com_Mirlo/salon3.jpg",
+      "/assets/commercials/Com_Mirlo/salon4.jpg",
+      "/assets/commercials/Com_Mirlo/salon_sup.jpg",
+      "/assets/commercials/Com_Mirlo/frente2.jpg",
+      "/assets/commercials/Com_Mirlo/cava.jpg",
+      "/assets/commercials/Com_Mirlo/cava2.jpg",
+    ],
+  },
+  {
+    categoria: "comerciales",
+    slug: "club-torcuato-tasso",
+    nombre: "Torcuato Tasso",
+    descripcion: `
+        Obra: Construcción local Torcuato Tasso
+        Dirección: Defensa 1575 – CABA
+        Trabajos realizados: Ejecución completa del local
+        Comitente: Torcuato Tasso
+        `,
+    thumbnail: "/assets/commercials/Com_Torcuato_Tasso/frente.jpg",
+    imagenes: [
+      "/assets/commercials/Com_Torcuato_Tasso/frente.jpg",
+      "/assets/commercials/Com_Torcuato_Tasso/salon.jpg",
+      "/assets/commercials/Com_Torcuato_Tasso/salon2.jpg",
+      "/assets/commercials/Com_Torcuato_Tasso/salon3.jpg",
+      "/assets/commercials/Com_Torcuato_Tasso/escenario.jpg",
+      "/assets/commercials/Com_Torcuato_Tasso/escenario2.jpg",
+      "/assets/commercials/Com_Torcuato_Tasso/barra.jpg",
+      "/assets/commercials/Com_Torcuato_Tasso/panoramica_salon.jpg",
+    ],
+  },
+];
